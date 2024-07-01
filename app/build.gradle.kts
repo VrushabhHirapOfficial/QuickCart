fe79ca57050329dc7516278779557a6ec2b1a5ce
Vrushabh Hirap <vrushabhhirap8@gmail.com>
2024-07-01 23:33:59 +0530

    Login activity implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,14 +37,13 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+
     implementation("com.google.android.material:material:1.3.0")
     implementation("com.google.android.material:material:1.5.0")
     implementation ("com.google.android.material:material:1.4.0")
     implementation ("androidx.appcompat:appcompat:1.3.1")
     implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation ("com.google.firebase:firebase-auth:21.0.1")
-
-
     implementation ("com.google.android.gms:play-services-auth:19.0.0")
     implementation ("com.facebook.android:facebook-login:11.3.0")
 
