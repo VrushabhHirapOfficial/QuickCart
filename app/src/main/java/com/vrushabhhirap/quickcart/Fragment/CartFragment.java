7ea10ab77db27ef585f2681f3dcc71447ed0b4ee
Hariprasad Satishkumar Korade <68178339+korade-hariprasad@users.noreply.github.com>
2024-07-09 18:45:42 +0530

    add to cart issue found. solution in CartFragment.java line 114 to 122

--- app/src/main/java/com/vrushabhhirap/quickcart/Fragment/CartFragment.java
+++ app/src/main/java/com/vrushabhhirap/quickcart/Fragment/CartFragment.java
@@ -111,6 +111,15 @@ public class CartFragment extends Fragment {
                                     myCartModel myCartModel1 = documentSnapshot.toObject(myCartModel.class);
                                     int tempTotalQuantity = Integer.parseInt( String.valueOf(documentSnapshot.getLong("totalQuantity")));
                                     myCartModel1.setTotalQuantity(String.valueOf(tempTotalQuantity)); // Pass String value
+                                    /*
+                                    //EDIT BY HARI -- 9-July -- START
+                                    //Issue is "documentSnapshot.getLong()", instead we need to use "documentSnapshot.getString()" as its already string in database.
+                                    //remove the tempTotalQuantity & set total quantity as below.
+
+                                    myCartModel1.setTotalQuantity(documentSnapshot.getString("totalQuantity"));
+
+                                    //EDIT BY HARI -- 9-July -- END
+                                    */
                                     cartModelList.add(myCartModel1);
                                     myCartAdapter.notifyDataSetChanged();
                                 }
